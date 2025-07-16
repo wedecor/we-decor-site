28dfdc6a85ca2fd7b42a89e2b99ffd927b9757f1
IlyasWaseem <96107598+IlyasWaseem@users.noreply.github.com>
2025-07-16 14:15:39 +0530

    fix: remove conflicting /robots.txt and /sitemap.xml from pages

--- pages/robots.txt.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.write("User-agent: *\nAllow: /\nSitemap: https://wedecor.in/sitemap.xml");
-  res.end();
-}
