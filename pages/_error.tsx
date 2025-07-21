d87ee789dcc293e839afda573623603264462490
IlyasWaseem <96107598+IlyasWaseem@users.noreply.github.com>
2025-07-21 20:08:33 +0530

    Fix TypeScript error in custom error page for production build

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,17 +1,19 @@
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
+import { NextPageContext } from "next";
 
-const CustomErrorComponent = (props) => {
-  return <Error statusCode={props.statusCode} />;
+interface ErrorProps {
+  statusCode?: number;
+}
+
+const CustomErrorComponent = (props: ErrorProps) => {
+  // Ensure statusCode is always a number
+  const statusCode = props.statusCode ?? 404;
+  return <Error statusCode={statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
-  // In case this is running in a serverless function, await this in order to give Sentry
-  // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
-
-  // This will contain the status code of the response
-  return Error.getInitialProps(contextData);
+CustomErrorComponent.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
 };
 
 export default CustomErrorComponent;
